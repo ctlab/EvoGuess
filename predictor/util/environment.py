b6b190f30c1ec7a507a845614f448fd8eb0eaddc
Артём Павленко <lytr777@mail.ru>
2019-10-02 20:08:37 +0300

    remove useless utils

--- predictor/util/environment.py
+++
@@ -1,9 +0,0 @@
-class Environment:
-    def __init__(self):
-        self.cnf = None
-        self.solvers = None
-        self.debugger = None
-        self.algorithm = None
-
-
-environment = Environment()
