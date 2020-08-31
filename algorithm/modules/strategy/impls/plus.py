298e27618b5fde5b7eae5346e1e43f9dd4a94c22
Артём Павленко <lytr777@mail.ru>
2020-08-31 04:16:34 +0300

    upd (m+l)

--- algorithm/modules/strategy/impls/plus.py
+++ algorithm/modules/strategy/impls/plus.py
@@ -9,11 +9,13 @@ class Plus(Strategy):
         self.name = 'Strategy: (%d + %d)' % (self.mu, self.lmbda)
 
     def tweak(self, generator: Iterable[Individual]) -> Tuple[Population, Population]:
-        size = max(self.mu, self.lmbda)
-        parents = [next(generator) for _ in range(size)]
-        children = map(self.mutation.mutate, parents[:self.lmbda])
+        # size = max(self.mu, self.lmbda)
+        # parents = [next(generator) for _ in range(size)]
+        # children = map(self.mutation.mutate, parents[:self.lmbda])
+        # return parents[:self.mu], list(children)
 
-        return parents[:self.mu], list(children)
+        parents = [next(generator) for _ in range(self.lmbda)]
+        return [], list(map(self.mutation.mutate, parents))
 
     def __len__(self):
         return self.mu + self.lmbda
