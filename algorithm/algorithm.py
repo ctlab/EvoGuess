c99cfbac3d96cf37b90aac6d3f06e765edd304d2
Артём Павленко <lytr777@mail.ru>
2020-11-01 10:48:01 +0300

    add iteration stamp and fix modules errors

--- algorithm/algorithm.py
+++ algorithm/algorithm.py
@@ -17,7 +17,8 @@ class Algorithm:
     def __init__(self,
                  limit: Limit,
                  method: Method,
-                 output: Output
+                 output: Output,
+                 *args, **kwargs
                  ):
         self.limit = limit
         self.method = method
@@ -35,23 +36,26 @@ class Algorithm:
         st_timestamp = now()
         self.limit.set('iteration', 0)
         population = self.initialize(backdoor)
-        self.limit.set('time', now() - st_timestamp)
-        self._log_iteration(0, population, now() - st_timestamp)
+        it_time = now() - st_timestamp
+        self.limit.set('time', it_time)
+        self._log_iteration(0, population, it_time, it_time)
 
         i = 1
         while not self.limit.exhausted():
             self.limit.set('iteration', i)
             it_timestamp = now()
             population = self.iteration(population)
-            self._log_iteration(i, population, now() - it_timestamp)
-            self.limit.set('time', now() - st_timestamp)
+            it_time = now() - it_timestamp
+            full_time = now() - st_timestamp
+            self.limit.set('time', full_time)
+            self._log_iteration(i, population, it_time, full_time)
             i += 1
 
         return population
 
-    def _log_iteration(self, it, population, time):
+    def _log_iteration(self, it, population, time, full_time):
         self.output.log(
-            'Iteration %d' % it,
+            'Iteration %d (stamp: %.2f)' % (it, full_time),
             'Individuals (%d):' % len(population),
             *['-- %s' % str(i) for i in population],
             'Time: %.2f' % time,
