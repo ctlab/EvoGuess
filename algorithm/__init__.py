62f1d05d51c24071ae66bc6103c26d8082527fc9
Артём Павленко <lytr777@mail.ru>
2020-12-25 05:50:31 +0300

    add tabu

--- algorithm/__init__.py
+++ algorithm/__init__.py
@@ -1,8 +1,17 @@
 from . import limit, evolution
+from .tabu_search import TabuSearch
 
 
 def get_algorithm(params):
-    return evolution.get_ea(params)
+    impl, kwargs = evolution.get_ea(params)
+    if impl is not None:
+        return impl, kwargs
+
+    kwargs = TabuSearch.parse(params)
+    if kwargs is not None:
+        return TabuSearch, kwargs
+    else:
+        return None, {}
 
 
 __all__ = [
